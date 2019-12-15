a3b9b53826069d10d90397bab4d2cf26eeb96ccf
ganalez2006 <ganalez2006@gmail.com>
2019-12-15 06:24:00 -0400

    show img

--- index.php
+++ index.php
@@ -1,15 +1,13 @@
-<?php #require_once('index.html') ?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+	<title>Download Images Instagram</title>
 
-<?php
-// tiempo maximo de ejecucion de php (s * m)
-set_time_limit(60 * 3);
-
-//$url = 'https://www.instagram.com/p/B3WRcIbg961/';
-$url = $_POST['url'];
-
-if (!isset($url) || empty($url)) {
-
-	?>
+	<meta name="apple-mobile-web-app-capable" content="yes" />
+	<meta name="apple-mobile-web-app-status-bar-style" content="black" />
+	
 	<style>
 	#url {
 		display: block;
@@ -19,42 +17,63 @@ if (!isset($url) || empty($url)) {
 		border-radius: 2px;
 		border: 1px solid #cdcdcd;
 	}
+	img {
+		display: block;
+		margin: auto;
+	}
 	</style>
-	<form action="" method="POST">
-		<label for="url">
-			Instagram Image url: <br>
-			<input type="text" name="url" id="url">
-		</label>
-	</form>
+</head>
+<body>
+
 	<?php
-	exit;
-}
+	// tiempo maximo de ejecucion de php (s * m)
+	set_time_limit(60 * 3);
 
-$url = 'https://opengraphcheck.com/result.php?url=' . urlencode($url);
+	//$url = 'https://www.instagram.com/p/B3WRcIbg961/';
+	$url = $_POST['url'];
 
-// Peticion curl
-$ch = curl_init($url);
-// solucionar "SSL certificate problem: unable to get local issuer certificate"
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-// No imprimir el contenido en pantalla
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$content = curl_exec($ch);
-$error = curl_error($ch);
-curl_close($ch);
+	if (!isset($url) || empty($url)) {
 
-// extraer las filas de la tabla
-preg_match_all("|<tr>(.*?)</tr>|si", $content, $parts);
+		?>
+		<form action="" method="POST">
+			<label for="url">
+				Instagram Image url: <br>
+				<input type="text" name="url" id="url">
+			</label>
+		</form>
+		<?php
+		exit;
+	}
 
-// fila de la imagen
-$img = $parts[1][3];
+	$url = 'https://opengraphcheck.com/result.php?url=' . urlencode($url);
 
-// expression regular para la url
-$pattern = '|(?<!")(?<!"\s)(https?:\/\/[^\s"\[<]+)|im';
-preg_match_all($pattern, $img, $parts);
+	// Peticion curl
+	$ch = curl_init($url);
+	// solucionar "SSL certificate problem: unable to get local issuer certificate"
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+	// No imprimir el contenido en pantalla
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	$content = curl_exec($ch);
+	$error = curl_error($ch);
+	curl_close($ch);
 
-// imagen
-$img = $parts[1][0];
-echo '<img src="'. $img . '" alt="" style="display:block;margin:auto;">';
+	// extraer las filas de la tabla
+	preg_match_all("|<tr>(.*?)</tr>|si", $content, $parts);
 
-exit;
+	// fila de la imagen
+	$img = $parts[1][3];
+
+	// expression regular para la url
+	$pattern = '|(?<!")(?<!"\s)(https?:\/\/[^\s"\[<]+)|im';
+	preg_match_all($pattern, $img, $parts);
+
+	// imagen
+	$img = $parts[1][0];
+	?>
+
+	<img src="<?= $img ?>" alt="">
+</body>
+</html>
+
+<?php #require_once('index.html') ?>
