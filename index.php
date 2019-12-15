57949ae1b3bc90e810caceba9d2061ac141077d6
ganalez2006 <ganalez2006@gmail.com>
2019-12-15 06:29:16 -0400

    show img

--- index.php
+++ index.php
@@ -23,6 +23,7 @@
 		display: block;
 		margin: 10px auto;
 		width: 100%;
+		max-width: 100%;
 		padding: 10px;
 		border-radius: 2px;
 		border: 1px solid #cdcdcd;
@@ -30,6 +31,7 @@
 	img {
 		display: block;
 		margin: auto;
+		max-width: 100%;
 	}
 	</style>
 </head>
@@ -42,47 +44,46 @@
 	//$url = 'https://www.instagram.com/p/B3WRcIbg961/';
 	$url = $_POST['url'];
 
-	if (!isset($url) || empty($url)) {
+	if (isset($url) && !empty($url)) {
 
+		$url = 'https://opengraphcheck.com/result.php?url=' . urlencode($url);
+
+		// Peticion curl
+		$ch = curl_init($url);
+		// solucionar "SSL certificate problem: unable to get local issuer certificate"
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+		// No imprimir el contenido en pantalla
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		$content = curl_exec($ch);
+		$error = curl_error($ch);
+		curl_close($ch);
+
+		// extraer las filas de la tabla
+		preg_match_all("|<tr>(.*?)</tr>|si", $content, $parts);
+
+		// fila de la imagen
+		$img = $parts[1][3];
+
+		// expression regular para la url
+		$pattern = '|(?<!")(?<!"\s)(https?:\/\/[^\s"\[<]+)|im';
+		preg_match_all($pattern, $img, $parts);
+
+		// imagen
+		$img = $parts[1][0];
 		?>
-		<form action="" method="POST">
-			<label for="url">
-				Instagram Image url: <br>
-				<input type="text" name="url" id="url">
-			</label>
-		</form>
-		<?php
-		exit;
+
+		<img src="<?= $img ?>" alt="">
 	}
-
-	$url = 'https://opengraphcheck.com/result.php?url=' . urlencode($url);
-
-	// Peticion curl
-	$ch = curl_init($url);
-	// solucionar "SSL certificate problem: unable to get local issuer certificate"
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-	// No imprimir el contenido en pantalla
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	$content = curl_exec($ch);
-	$error = curl_error($ch);
-	curl_close($ch);
-
-	// extraer las filas de la tabla
-	preg_match_all("|<tr>(.*?)</tr>|si", $content, $parts);
-
-	// fila de la imagen
-	$img = $parts[1][3];
-
-	// expression regular para la url
-	$pattern = '|(?<!")(?<!"\s)(https?:\/\/[^\s"\[<]+)|im';
-	preg_match_all($pattern, $img, $parts);
-
-	// imagen
-	$img = $parts[1][0];
 	?>
 
-	<img src="<?= $img ?>" alt="">
+	<form action="" method="POST">
+		<label for="url">
+			Instagram Image url: <br>
+			<input type="text" name="url" id="url">
+		</label>
+	</form>
+
 </body>
 </html>
 
