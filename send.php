6fd2d5a85ec2837a5f7f045999deefba78c8cf42
ganalez2006 <ganalez2006@gmail.com>
2020-01-27 03:58:17 -0400

    test

--- send.php
+++ send.php
@@ -1,23 +1,76 @@
 <?php
 // API access key from Google API's Console
-define('API_ACCESS_KEY','AIzaSyCuMQvWQDrPlsx4g24Ym2RNo3jd5Ts2bkE');
+define('API_ACCESS_KEY','AAAANSnda04:APA91bEyi6awZtp-8eO6hyTWJBfIKH8S-5nyD9dl0p8d_NImGn8I8nx9dqz2xe8kxx7Tf6PsiuMS__9B94miaBLOXcgQoQL_XLFCGoHWDRPbro6GOOcQgkvz9KPMVpt_gqJtnQnZpued');
 
 $url = 'https://fcm.googleapis.com/fcm/send';
 
-//$registrationIds = array($_GET['id']);
+$title = '@helmet_indonesia';
+$tag = 'tag-' . time();
+$icon = 'https://scontent-mia3-1.cdninstagram.com/v/t51.2885-19/s150x150/30078221_372310423248218_4337383886209155072_n.jpg?_nc_ht=scontent-mia3-1.cdninstagram.com&_nc_ohc=hpZxfLhIEK0AX99Ee3G&oh=e6ed36b1438a246b2fa172fd5ff32917&oe=5ECE6CE6';
+$image = 'https://scontent-mia3-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/s750x750/82018619_782986595445444_8634981211527501189_n.jpg?_nc_ht=scontent-mia3-1.cdninstagram.com&_nc_cat=100&_nc_ohc=2b9AI32vMFQAX8J67XA&oh=c41f1cdb44c76386f232193be7d7702e&oe=5ECD0B4D';
+$body = 'Sunday vibes';
 
-// prepare the message
-$message = array( 
-	'title'     => 'This is a title.',
-	'body'      => '{"title":"bikegirlparis","options":{"body":"What do you think about my new equipment?","tag":"push-1580105438977","vibrate":[200,100,200],"renotify":true,"icon":"./images/icons/launcher-icon-1x.png","image":"https://scontent-mia3-1.cdninstagram.com/v/t51.2885-15/e35/81773741_156712045749336_1405689013483310911_n.jpg?_nc_ht=scontent-mia3-1.cdninstagram.com&_nc_cat=100&_nc_ohc=Xd0nAUOfRB0AX_mTvY-&oh=c9ee02d7f25a1b8f58d6aec9cb4160ef&oe=5ED16BC4","actions":[{"action":"reply","title":"Responder","type":"text","placeholder":"Escribe tu respuesta"},{"action":"action","title":"Me interesa","type":"button"}],"data":{"id":9999,"url":"https://google.com"}}}',
-	'vibrate'   => 1,
-	'sound'      => 1
-);
+$actions = array(
+	'button-button' => array(
+		array(
+			'action' => 'action-1'
+			, 'title' => 'Boton 1'
+			, 'type' => 'button'
+			)
+		, array(
+			'action' => 'action2'
+			, 'title' => 'Boton 2'
+			, 'type' => 'button'
+			)
+		)
+	, 'text-button' => array(
+		array(
+			'action' => 'reply'
+			, 'title' => 'Responder'
+			, 'type' => 'text'
+			, 'placeholder' => 'Escribe tu respuesta'
+			)
+		, array(
+			'action' => 'action-1'
+			, 'title' => 'Boton 1'
+			, 'type' => 'button'
+			)
+		)
+	);
+
+$data = array(
+	'id' => 9999
+	, 'url' => 'https://google.com'
+	);
+
+$body = array(
+	'title' => $title
+	, 'options' => array(
+		'body' => $body
+		, 'tag' => $tag
+		, 'vibrate' => [200, 100, 200]
+		, 'renotify' => true
+		, 'icon' => $icon
+		, 'image' => $image
+		//, 'actions' => $actions['button-button']
+		, 'actions' => $actions['text-button']
+		, 'data' => $data
+		)
+	);
+
+$to = [];
+$to['redmi'] = 'fTQIiN665_z6AuaWsarX6s:APA91bHIliwGYXaminX9bGxpdSucIWrt4p1CB53GkPhOWNnBsQSd7oNAZ18_z6U7G6pT4LR5oaYBj6f_lhVhrFYub1pdXXW3AwE8jLxkk0gHTF335SAK3PYZJByzBVbQOa9p0vo5Eo4R';
+$to['laptop'] = 'cjdd1DB4eY0w3cWA05IGs0:APA91bHhEMla0FS_oJa_ATjuSUGUKzLt9lzRtJsHbKpoUWZ3jBakCNRNI4ay5VWDX0PCCiJ64WSWEj_tTv-3oOWABuhYtMo5M-OGjbL91qyZSGRnq4hAMwy-95gUSesKyqDD9v0naI2Q';
+
+
 
 $fields = array( 
-	//'registration_ids' => $registrationIds, 
-	'data'             => $message
-);
+	'notification' => array(
+		'body' => json_encode($body)
+		)
+	, 'to' =>  $to['laptop']
+	, 'to' =>  $to['redmi']
+	);
 
 $headers = array( 
 	'Authorization: key='.API_ACCESS_KEY, 
